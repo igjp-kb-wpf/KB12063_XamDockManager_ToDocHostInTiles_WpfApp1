7d76331c71f5a228de494102e0fd320f66594127
norikois <33947244+norikois@users.noreply.github.com>
2023-10-20 19:01:28 +0900

    Initial code commit

---
+++ KB12063_WpfApp1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KB12063_WpfApp1;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
